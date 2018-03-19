f261e898360c3bedd98290303d4820ea7028fb3a
ecuber <unconfigured@null.spigotmc.org>
2018-03-19 19:45:45 -0400

    v1.1
    Scramblers included:
    2x2
    3x3
    4x4
    5x5
    Pyraminx
    Megaminx
    Square 1
    Skewb

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Scrambler-Bot
-A discord bot that generates Rubik's Cube scrambles. Using Nick Colley's Scrambo library on node.js. 
-You may do whatever you like with this bot, but I would appreciate you crediting me, ECuber. 
+An original Discord bot that generates Rubik's Cube scrambles. Made for the U.S. Cubers Server. Using Nick Colley's Scrambo library on node.js.
+The library: https://github.com/nickcolley/scrambo
+The server: https://discord.gg/nmaNvSn
+You may do whatever you like with this bot, but I would appreciate you crediting me, ECuber.
 Thanks for showing your interest in my first discord bot!
--- app.js
+++ app.js
@@ -20,51 +20,81 @@ bot.on('message', message => {
       message.channel.send("Simply type !scramble(2/3/4/5). !scramble also works to generate a 3x3 scramble.") //sends message to channel where command was sent originally
   }
 
-  //SCRAMBLE2 command
-    if (msg === prefix + 'SCRAMBLE2') {
-      // Generate a new 2x2 scramble
-      var seeded_scramble = new Scrambo().type('222').length(10).get();
-      console.log(seeded_scramble);
-      message.channel.send(seeded_scramble); //sends message to channel where command was sent originally
+        //SCRAMBLE2 command
+          if (msg === prefix + 'SCRAMBLE2') {
+              // Generate a new 2x2 scramble
+              var seeded_scramble = new Scrambo().type('222').length(10).get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
       }
 
-  //SCRAMBLE3 command
-    if (msg === prefix + 'SCRAMBLE') {
-      // Generate a new 3x3 scramble
-      var seeded_scramble = new Scrambo().type('333').length(20).get();
-      console.log(seeded_scramble);
-      message.channel.send(seeded_scramble); //sends message to channel where command was sent originally
+        //SCRAMBLE3 command
+          if (msg === prefix + 'SCRAMBLE') {
+              // Generate a new 3x3 scramble
+              var seeded_scramble = new Scrambo().type('333').get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
     }
 
-  //SCRAMBLE3 command
-    if (msg === prefix + 'SCRAMBLE3') {
-      // Generate a new 3x3 scramble
-      var seeded_scramble = new Scrambo().type('333').length(20).get();
-      console.log(seeded_scramble);
-      message.channel.send(seeded_scramble); //sends message to channel where command was sent originally
+        //SCRAMBLE3 command
+          if (msg === prefix + 'SCRAMBLE3') {
+              // Generate a new 3x3 scramble
+              var seeded_scramble = new Scrambo().type('333').get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
     }
 
-  //SCRAMBLE4 command
-    if (msg === prefix + 'SCRAMBLE4') {
-      // Generate a new 4x4 scramble with the seed of 1
-      var seeded_scramble = new Scrambo().type('444').length(30).get();
-      console.log(seeded_scramble);
-      message.channel.send(seeded_scramble); //sends message to channel where command was sent originally
-      }
+        //SCRAMBLE4 command
+          if (msg === prefix + 'SCRAMBLE4') {
+              // Generate a new 4x4 scramble with the seed of 1
+              var seeded_scramble = new Scrambo().type('444').length(30).get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
+    }
 
-  //SCRAMBLE5 command
-      if (msg === prefix + 'SCRAMBLE5') {
-      // Generate a new 5x5 scramble
-      var seeded_scramble = new Scrambo().type('555').length(35).get();
-      console.log(seeded_scramble);
-      message.channel.send(seeded_scramble); //sends message to channel where command was sent originally
-      }
+        //SCRAMBLE5 command
+            if (msg === prefix + 'SCRAMBLESKEWB') {
+              // Generate a new 5x5 scramble
+              var seeded_scramble = new Scrambo().type('skewb').get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
+    }
 
-});
+        //SCRAMBLESKEWB command
+            if (msg === prefix + 'SCRAMBLE5') {
+              // Generate a new Skewb scramble
+              var seeded_scramble = new Scrambo().type('555').length(36).get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
+    }
 
+        //SCRAMBLEPYRA command
+            if (msg === prefix + 'SCRAMBLEPYRA') {
+              // Generate a new Pyra scramble
+              var seeded_scramble = new Scrambo().type('pyram').get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
+    }
+
+        //SCRAMBLEMEGA command
+            if (msg === prefix + 'SCRAMBLEMEGA') {
+              // Generate a new Megaminx scramble
+              var seeded_scramble = new Scrambo().type('minx').get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
+    }
+
+        //SCRAMBLESQ1 command
+            if (msg === prefix + 'SCRAMBLESQ1') {
+              // Generate a new Sq1 scramble
+              var seeded_scramble = new Scrambo().type('sq1').get();
+              console.log(seeded_scramble);
+              message.channel.send(seeded_scramble); //sends scramble to channel where command was sent
+    }
+})
 //listener event: Bot launched
 bot.on('ready', () => {
-  console.log('Initialization complete.') // runs when bot is launched
+  console.log('Initialization complete!') // runs when bot is launched
 
 //status
 bot.user.setStatus('Online')
